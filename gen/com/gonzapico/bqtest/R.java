806da191a0cf5b56f27b32d4f6878773c5eac8c6
Gonzalo Fdez <gonzalo.fernandez.pico@gmail.com>
2014-06-09 19:13:10 +0200

    Pantalla de login funcionando

--- gen/com/gonzapico/bqtest/R.java
+++ gen/com/gonzapico/bqtest/R.java
@@ -10,11 +10,101 @@ package com.gonzapico.bqtest;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int action_forgot_password=0x7f080009;
+        public static final int action_settings=0x7f08000a;
+        public static final int email=0x7f080003;
+        public static final int esdk__switch_service=0x7f080008;
+        public static final int esdk__webview=0x7f080007;
+        public static final int login=0x7f080005;
+        public static final int login_form=0x7f080002;
+        public static final int login_status=0x7f080000;
+        public static final int login_status_message=0x7f080001;
+        public static final int password=0x7f080004;
+        public static final int sign_in_button=0x7f080006;
+    }
+    public static final class layout {
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int esdk__webview=0x7f030002;
+    }
+    public static final class menu {
+        public static final int esdk__oauth=0x7f070000;
+        public static final int login=0x7f070001;
+        public static final int main=0x7f070002;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int CANNOT_GET_LINKED_NOTEBOOK=0x7f04002a;
+        public static final int CANT_LIST_APP_LNB=0x7f04002b;
+        public static final int ERROR_GETTING_LINKED_NOTEBOOK=0x7f04002c;
+        public static final int action_forgot_password=0x7f040032;
+        public static final int action_settings=0x7f040009;
+        public static final int action_sign_in_register=0x7f040030;
+        public static final int action_sign_in_short=0x7f040031;
+        public static final int app_name=0x7f040007;
+        public static final int content=0x7f04001d;
+        public static final int empty_content_error=0x7f04001e;
+        public static final int err_creating_note=0x7f04000b;
+        public static final int err_image_selected=0x7f040011;
+        public static final int err_retrieving_resource=0x7f040012;
+        public static final int error_creating_notestore=0x7f040027;
+        public static final int error_field_required=0x7f040037;
+        public static final int error_incorrect_password=0x7f040036;
+        public static final int error_invalid_email=0x7f040034;
+        public static final int error_invalid_password=0x7f040035;
+        public static final int error_listing_notebooks=0x7f040026;
+        public static final int error_listing_notes=0x7f040029;
+        public static final int error_saving_note=0x7f040028;
+        public static final int esdk__err_api_setup=0x7f040005;
+        public static final int esdk__err_authentication=0x7f040004;
+        public static final int esdk__err_protocol_version=0x7f040003;
+        public static final int esdk__evernote=0x7f040000;
+        public static final int esdk__evernote_login_failed=0x7f040001;
+        public static final int esdk__evernote_login_successful=0x7f040002;
+        public static final int esdk__image_picker=0x7f040018;
+        public static final int esdk__loading=0x7f040006;
+        public static final int esdk__pick_notebook=0x7f040014;
+        public static final int esdk__search_notes=0x7f04001a;
+        public static final int esdk__simple_note=0x7f040019;
+        public static final int evernote_login=0x7f04000a;
+        public static final int image_picker=0x7f040023;
+        public static final int label_app_title=0x7f04000d;
+        public static final int label_login=0x7f040015;
+        public static final int label_logout=0x7f040016;
+        public static final int label_message=0x7f04000f;
+        public static final int label_save_button=0x7f040010;
+        public static final int label_search=0x7f040017;
+        public static final int label_select_button=0x7f04000e;
+        public static final int label_select_notebook_button=0x7f04001b;
+        public static final int login_progress_signing_in=0x7f040033;
+        public static final int msg_image_saved=0x7f04000c;
+        public static final int msg_retrieved_resource=0x7f040013;
+        public static final int note_saved=0x7f04001f;
+        public static final int notes_searched=0x7f040020;
+        public static final int ok=0x7f040021;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f04002e;
+        public static final int prompt_password=0x7f04002f;
+        public static final int search_hint=0x7f040025;
+        public static final int search_notes=0x7f040024;
+        public static final int simple_note=0x7f040022;
+        public static final int title=0x7f04001c;
+        public static final int title_activity_login=0x7f04002d;
+        public static final int title_activity_main=0x7f040008;
     }
     public static final class style {
         /** 
@@ -38,10 +128,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
+        public static final int LoginFormContainer=0x7f060002;
     }
 }
